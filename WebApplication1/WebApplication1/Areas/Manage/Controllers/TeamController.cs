629192889c605b5415a2f05c3b65e5cfba519a89
Dunyamali Ahmadli <tu7bg7w6d@aztu.local>
2022-06-30 12:56:23 +0400

    added trask file

---
+++ WebApplication1/WebApplication1/Areas/Manage/Controllers/TeamController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.DAL;
+using WebApplication1.Models;
+
+namespace WebApplication1.Areas.Manage.Controllers
+{
+    [Area("Manage")]
+    public class TeamController : Controller
+    {
+        private AppDbContext _context { get; }
+        private IWebHostEnvironment _env { get; }
+        public TeamController(AppDbContext context , IWebHostEnvironment env)
+        {
+            _context = context;
+            _env = env;
+        }
+        public IActionResult Index()
+        {
+            var page = _context.Teams.ToList();
+            return View(page);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Team team)
+        {
+            if (!ModelState.IsValid) return NotFound();
+            if (team.Photo != null)
+            {
+                if (team.Photo.ContentType != "image/jpg" && team.Photo.ContentType != "image/png")
+                {
+                    ModelState.AddModelError("", "Fayll png ve ya jpg olmalidir");
+                    return View(team);
+                }
+              
+                string filename = team.Photo.FileName;
+                if (filename.Length > 64)
+                {
+                    filename.Substring(filename.Length - 64, 64);
+
+                }
+                string newFileName = Guid.NewGuid().ToString() + filename;
+                string path = Path.Combine(_env.WebRootPath,"assets","img",newFileName);
+                using (FileStream fs = new FileStream(path,FileMode.Create))
+                {
+                    team.Photo.CopyTo(fs);
+                }
+                team.Image = newFileName;
+                _context.Teams.Add(team);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int Id) {
+            var deleted = _context.Teams.Find(Id);
+            if (deleted == null) return NotFound();
+            _context.Teams.Remove(deleted);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+                }
+
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Team.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Team
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string JobName { get; set; }
+
+        public string Image { get; set; }
+
+        [NotMapped]
+        public IFormFile Photo { get; set; }
+    }
+
+    
+}
